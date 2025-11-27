36865cd277e5b3286d84399dc0aaeb3aec06bbd6
abudiayu <abudiayuu@gmail.com>
2025-11-27 16:12:19 +0300

    Category

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./index.css"
 import './App.css';
 import Header from './components/Header/Header';
 import Carousel from "./components/Carousel/CarouselEffect"
+import Category from "./components/Category/Category";
 
 function App() {
   return (
     <>
       <Header/>
       <Carousel/>
+      <Category/>
     </>
   );
 }
---
+++ src/components/Category/CategoryFullInfo.js
@@ -0,0 +1,26 @@
+export const CategoryInfo = [
+  {
+    title: "Electronics",
+    name: "Smartphones",
+    imgLink: 
+      "https://m.media-amazon.com/images/I/61qvRagAn9L._AC_UY218_.jpg"
+  },
+  {
+    title: "Home Appliances",
+    name: "Vacuum Cleaners",
+    imgLink: 
+        "https://m.media-amazon.com/images/I/611S4FYZhsL._AC_SY300_SX300_QL70_FMwebp_.jpg"
+  },
+  {
+    title: "Fashion",
+    name: "Women's Jackets",
+    imgLink:
+         "https://m.media-amazon.com/images/I/61bGByplOqL._AC_SY550_.jpg"
+  },
+  {
+    title: "Man's Clothing",
+    name: "men's clothing",
+    imgLink: 
+        "https://m.media-amazon.com/images/I/71thhD4ajQL._AC_UL320_.jpg"
+  }
+];

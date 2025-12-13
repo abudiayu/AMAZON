d9e4f798e240e7242eaea81ec410ff75284a0674
abudiayu <abudiayuu@gmail.com>
2025-12-13 14:32:06 +0300

    payment by creadit card

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,39 @@
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
-const express = require("express")
-const cors =require("cors")
-const dotenv= require("dotenv")
-dotenv.config();
-const stripe = require("stripe")(process.env.STRIPE_KEY)
+const { setGlobalOptions } = require("firebase-functions/v2");
+const { onRequest } = require("firebase-functions/v2/https");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
+dotenv.config();
 setGlobalOptions({ maxInstances: 10 });
 
-const app = express()
-app.use(cors({origin:true}))
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-app.use(express.json())
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-app.get("/",(req, res)=>{
-    res.status(200).json({
-        message:"Success abudy!",
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success abudy!",
+  });
+});
+app.post("/payment/create",async (req,res) => {
+    const total = req.query.total;
+    if(total > 0){
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount:total,
+            currency:"USD",
+        });
+        console.log(paymentIntent)
+        res.status(201).json({
+            clientSecret:paymentIntent.client_secret,
+        });
+    }else{
+        res.status(403).json({
+            message:"total must be above 0",
+        });
+    }
+});
 
 exports.api = onRequest(app);
-
-// must encodeURIComponent. 
